2001c9f3b8afb51051adbcc399bb28be38893d8c
Tsurkan <mikh.tsurkan@gmail.com>
2024-02-19 18:41:32 +0300

    add is not autheticated test mixin with no hardcoding

--- services/users/mixins.py
+++ services/users/mixins.py
@@ -4,9 +4,27 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.urls import reverse
 from django.contrib.auth import get_user_model, login
-
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
 
 User = get_user_model()
+
+
+class UserIsNotAuthenticatedMixin(UserPassesTestMixin):
+    error_is_authenticated_message = None
+    error_is_authenticated_redirect_viewname = None
+    request = None
+
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            messages.info(self.request, self.error_is_authenticated_message)
+            raise PermissionDenied
+        return True
+
+    def handle_no_permission(self):
+        return redirect(self.error_is_authenticated_redirect_viewname)
 
 
 class ValidationMessageMixin:
